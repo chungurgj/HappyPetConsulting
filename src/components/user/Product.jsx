87c1daf4f599453d2ce9079826cec3eef855e55b
Gjorgji Chungurski <gjorgji@chungurski.mk>
2024-06-12 19:40:01 +0200

    cc

--- src/components/user/Product.jsx
+++ src/components/user/Product.jsx
@@ -73,36 +73,39 @@ const Product = (props) => {
   },[props.item,props.category,props.animal ])
 
   return (
-    <div className="product-container">
-      {dbProducts && dbProducts.map(curElm=>(
-        <div className="product-box" key={curElm.id}>
-        <div className="product-content">
-          <div className="product-img">
-            <img src={curElm.imgURL} height={200} width={200} alt={curElm.title} className='imgimg'/>
-          </div>
-          <div className="detail">
-            <div className="product-info">
-              <h4 className="title">{curElm.title}</h4>
-              <p className="cena">{curElm.price} денари</p>
-            </div>
-            <div className='productgroup'>
-              <button onClick={() => handleChosenProduct(curElm.title, curElm.id)} className="btn btn-danger ">Кошничка</button>
+    // <div className="product-container">
+    //   {dbProducts && dbProducts.map(curElm=>(
+    //     <div className="product-box" key={curElm.id}>
+    //     <div className="product-content">
+    //       <div className="product-img">
+    //         <img src={curElm.imgURL} height={200} width={200} alt={curElm.title} className='imgimg'/>
+    //       </div>
+    //       <div className="detail">
+    //         <div className="product-info">
+    //           <h4 className="title">{curElm.title}</h4>
+    //           <p className="cena">{curElm.price} денари</p>
+    //         </div>
+    //         <div className='productgroup'>
+    //           <button onClick={() => handleChosenProduct(curElm.title, curElm.id)} className="btn btn-danger ">Кошничка</button>
 
-              <input
-                onChange={(e) => handleQuantity(curElm.id, e.target.value)}
-                className='form-control quantity-button'
-                type="number"
-                placeholder='0'
-                value={productQuantities[curElm.id] || 0}
-              />
-            </div>
+    //           <input
+    //             onChange={(e) => handleQuantity(curElm.id, e.target.value)}
+    //             className='form-control quantity-button'
+    //             type="number"
+    //             placeholder='0'
+    //             value={productQuantities[curElm.id] || 0}
+    //           />
+    //         </div>
 
-          </div>
-        </div>
-      </div>
-      ))}
+    //       </div>
+    //     </div>
+    //   </div>
+    //   ))}
 
 
+    // </div>
+    <div className="productsContainer">
+      
     </div>
   );
 };
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,8 +3,7 @@ import Product from '../components/user/Product'
 import Search from '../img/icons8-search-48.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { toast } from 'react-toastify';
-import axios from 'axios';
+import ShopSidebar from '../components/user/ShopSidebar';
 const Shop = () => {
 
   const [selectedCategory,setSelectedCategory] = useState('site')
@@ -35,40 +34,8 @@ const Shop = () => {
   
   return(
     <div className='shop-container'>
+      <ShopSidebar/>
       <Product category={selectedCategory} item={searchItem} animal={selectedAnimal} />
-      <div className='shop-sidebar'>
-          <div className="input-group">
-            <div className="form-outline" data-mdb-input-init>
-              <input type="search" id="form1" className="form-control disappear reduce" onChange={handleSearchItem} placeholder='Пребарај на кирилица'/>
-            </div>
-        </div>
-          <h5 className='disappear'>Категорија </h5>
-          <nav aria-label="breadcrumb">
-            <ol className="breadcrumb disappear">
-              <li className="breadcrumb-item active" aria-current="page">Продавница</li>
-            </ol>
-          </nav>
-          
-          <ul className='sidebar-list disappear'>
-            <li className={`${selectedCategory === 'site' ? 'active': ''}`} ><a onClick={()=>handleCategory('site')} href='#'>Сите</a></li>
-            <li className={`${selectedCategory === 'hrana' ? 'active': ''}`} ><a onClick={()=>handleCategory('hrana')}>Храна</a></li>
-            <li className={`${selectedCategory === 'lekovi' ? 'active': ''}`}><a onClick={()=>handleCategory('lekovi')}>Лекови</a></li>
-            <li className={`${selectedCategory === 'odrzuvanje' ? 'active': ''}`}><a onClick={()=>handleCategory('odrzuvanje')}>Одржување</a></li>
-            <li className={`${selectedCategory === 'galanterija' ? 'active': ''}`}><a onClick={()=>handleCategory('galanterija')}>Галантерија</a></li>
-          </ul>
-
-          <select value={selectedAnimal} onChange={(e) => handleSelectedAnimal(e)} className='form-select myselect disappear reduce'>
-              <option value='site'>Одберете милениче</option>
-              <option value="kuce">Куче</option>
-              <option value="macka">Мачка</option>
-              <option value="zajak">Зајак</option>
-              <option value="papagal">Папагал</option>
-              <option value="ribi">Риби</option>
-              <option value="ostanato">Останато</option>
-        </select>
-
-        
-      </div>
     </div>
   )
 }
