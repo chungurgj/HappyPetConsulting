f9bed7474509568e7d1ccdf0d5608a22ed1f1995
Gjorgji Chungurski <gjorgji@chungurski.mk>
2024-06-13 13:44:34 +0200

    save

--- src/pages/ConsultationNavigator.jsx
+++ src/pages/ConsultationNavigator.jsx
@@ -3,47 +3,16 @@ import { useNavigate } from 'react-router-dom'
 import Emergency from '../img/emergency.png'
 import Text from '../img/text.png'
 import Video from '../img/camera.png' 
-
+import ConsNavComp from '../components/ConsNavComp'
 const Consultation = () => {
 
   const navigate = useNavigate()
 
   return ( 
-      <div className='cons-nav-main'>
-        <div className='cons-nav-content'>
-        <div className='consult'>
-        <img src={Video} height={48} width={48}/>
-          <h5>Видео советување</h5>
-          <div className='align-consult'>
-            <small>Добивате детална консутлација на вашето милениче преку видео повик, најпогодно кога ситуацијата не е
-              живото-загрозувачка и немате пристап до добар ветеринар.
-            </small>
-            <button className='btn btn-success' onClick={()=>navigate('/consultation/video')}>Продолжи</button>
-          </div>
-          
-        </div>
-        <div className='consult'>
-          <img src={Emergency} height={48} width={48}/>
-          <h5>Итно советување</h5>
-          <div className='align-consult emerg'>
-            <small>Добивате консултација во рок од 15 минути од плаќањето, со јасни инструкции за спасување на вашето милениче додека да добиете 
-              професионална помош. Најпогодно во живото-загрозувачки ситуации додека да добиете пристап до ветеринар.
-            </small>
-            <button className='btn btn-success ' onClick={()=>navigate('/consultation/emergency')}>Продолжи</button>
-          </div>
-          
-        </div>
-        <div className='consult'>
-        <img src={Text} height={48} width={48}/>
-          <h5>Текстуално советување</h5>
-          <div className='align-consult'>
-            <small>Можете да поставите најразлични прашања за вашето милениче, исхраната, лекови и слично. Добивате повратен одговор во рок од 1 час.
-            </small>
-            <button className='btn btn-success' onClick={()=>navigate('/consultation/text')}>Продолжи</button>
-          </div>
-         
-        </div>
-      </div>
+      <div className="consNavContainer">
+        <ConsNavComp imgUrl={Video} title={'Видео советување'} text={'Добивате детална консутлација на вашето милениче преку видео повик, најпогодно кога ситуацијата не е живото-загрозувачка и немате пристап до добар ветеринар.'}/>
+        <ConsNavComp imgUrl={Emergency} title={'Итно советување'} text={'Добивате консултација во рок од 15 минути од плаќањето, со јасни инструкции за спасување на вашето милениче додека да добиете професионална помош. Најпогодно во живото-загрозувачки ситуации додека да добиете пристап до ветеринар.'}/>
+        <ConsNavComp imgUrl={Text} title={'Текстуално советување'} text={'Можете да поставите најразлични прашања за вашето милениче, исхраната, лекови и слично. Добивате повратен одговор во рок од 1 час.'}/>
       </div>
   )
 }
